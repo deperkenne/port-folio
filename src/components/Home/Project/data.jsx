7633405a4c183216c7d38060cf2bcf7a475ab883
deperkenne <tchofodep@gmail.com>
2024-07-06 09:05:55 +0200

    change url

--- src/components/Home/Project/data.jsx
+++ src/components/Home/Project/data.jsx
@@ -9,7 +9,7 @@ export const projets = [
       code:"Through this link, you can directly access my source code on GitHub."
     },
     {
-      url: "https://github.com/issuetrackingapp",
+      url: "https://github.com/deperkenne/issuetrackingapp.git",
       img: "issuetrackingapp.png",
       title: "Issue Tracking System",
       text: "Implementation of an Issue Tracking And Developed a user-friendly console interface for system interaction",
@@ -18,7 +18,7 @@ export const projets = [
     },
     
     {
-      url: "https://github.com/deperkenne/port-folio.git",
+      url: "https://github.com/deperkenne",
       img: "junittest.png",
       title: "Enhancement and Extension of the Billing System",
       text: "To realize this application, I studied Chapter 1 of Martin Fowler's book to understand the structure and logic of the existing pricing and billing system. ",
@@ -26,7 +26,7 @@ export const projets = [
      
     },
     {
-      url:"https://github.com/deperkenne/port-folio.git",
+      url:"https://github.com/deperkenne/",
       img: "restapi.png",
       title: "Building a Java REST API",
       text: " ",
